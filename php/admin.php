d529b6fea5af261d50ff5fef1cdebba458429097
John Morrison <G20M7040@ict.ru.ac.za>
2022-05-21 12:12:08 +0200

    added sql to create table in admin.php, added name and surname to signup page

--- php/admin.php
+++ php/admin.php
@@ -14,13 +14,41 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-$sql = "CREATE TABLE MyGuests (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  firstname VARCHAR(30) NOT NULL,
-  lastname VARCHAR(30) NOT NULL,
-  email VARCHAR(50),
-  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  )";
+$sqlCreateAttractionsTable = "CREATE TABLE Attractions (
+                              AttractionId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                              Title VARCHAR(30) DEFAULT 'Title',
+                              Descriptions VARCHAR(280) DEFAULT 'Add Description',
+                              Likes INT(255),
+                              CHECK (Likes >-1)
+                              )";
+
+$sqlCreateUsersTable = "CREATE TABLE Users (
+                        UserId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        Username VARCHAR(30) NOT NULL UNIQUE,
+                        Name VARCHAR(30) NOT NULL,
+                        Surname VARCHAR(30) NOT NULL,
+                        Email NVARCHAR (255) NOT NULL,
+                        Password VARCHAR(50) NOT NULL /*this isn't right need to come up with a better Type */
+                        )";
+
+$sqlCreatereviewTable = "CREATE TABLE Review (
+                         ReviewId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                         UserId INT(6) NOT NULL,
+                         ReviewDescription VARCHAR(280) DEFAULT 'Add Review',
+                         FOREIGN KEY (UserId) REFERENCES Users(UserId)
+                         )";
+
+$sqlCreateUserLoginTable = "CREATE TABLE UserLogin (
+                           Username VARCHAR(30) NOT NULL UNIQUE,
+                           Password VARCHAR(50) NOT NULL, /*this isn't right need to come up with a better Type */
+                           FOREIGN KEY (UserName) REFERENCES Users(UserId),
+                           FOREIGN KEY (Password) REFERENCES Users(Password)
+                           )";
+/*Not sure about this last table */
+$sqlEmailTable = "CREATE TABLE NewsLetterEmail (
+                               EmailId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                               Email NVARCHAR (255) NOT NULL
+)";
 
 if ($conn->query($sql) === TRUE) {
   echo "Table MyGuests created successfully";
