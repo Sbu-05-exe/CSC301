dd747f2dcb8b082ebe38d2c394570aa29150a4b6
Sibusiso Dlamini <g20d9300@campus.ru.ac.za>
2022-05-24 19:12:57 +0200

    admin page rolled back for some reason so i redid it

--- php/admin.php
+++ php/admin.php
@@ -1,5 +1,6 @@
+102 lines (79 sloc) 3.4 KB
 <?php
-// $servername = "Z:\Apache24\htdocs\Practicals\G20D9300\WebTech-Practical\php\info.php";
+
 $servername = "cs3-dev.ict.ru.ac.za";
 $username = "G20D9300";
 $password = "G20D9300";
@@ -12,50 +13,91 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-//SQL for creating table
+echo "Connected successfully <br/>";
+
+// $sqlTestingCreateTableQuery = "CREATE TABLE temp (
+  //                     tempID INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY 
+  //                     )";
+  
+  //SQL for creating tables
 $sqlCreateAttractionsTable = "CREATE TABLE Attractions (
-                              AttractionId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                              Title VARCHAR(30) DEFAULT 'Title',
-                              Descriptions VARCHAR(280) DEFAULT 'Add Description',
+                              AttractionID INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                              Title VARCHAR(30),
+                              Descriptions VARCHAR(280),
                               Likes INT(255),
                               CHECK (Likes >-1)
                               )";
 
 $sqlCreateUsersTable = "CREATE TABLE Users (
-                        UserId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        UserID INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
                         Username VARCHAR(30) NOT NULL UNIQUE,
-                        Name VARCHAR(30) NOT NULL,
-                        Surname VARCHAR(30) NOT NULL,
-                        Email NVARCHAR (255) NOT NULL,
-                        Password VARCHAR(50) NOT NULL, /*this isn't right need to come up with a better Type */
-                        UserType BIT(1) NOT NULL
+                        FirstName VARCHAR(30) NOT NULL,
+                        LastName VARCHAR(30) NOT NULL,
+                        Email VARCHAR (255) NOT NULL,
+                        PasswordHash VARCHAR(50) NOT NULL 
                         )";
+                        // PasswordHash BINARY(50) NOT NULL (this is the goal but for now i'm not really worried about security practicies)
 
-$sqlCreatereviewTable = "CREATE TABLE Review (
-                         ReviewId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                         UserId INT(6) NOT NULL,
-                         ReviewDescription VARCHAR(280) DEFAULT 'Add Review',
-                         FOREIGN KEY (UserId) REFERENCES Users(UserId)
+   /*this isn't right need to come up with a better Type */ 
+
+$sqlCreateReviewTable = "CREATE TABLE Reviews (
+                         ReviewID INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                         UserID INT(6) NOT NULL,
+                         AttractionID INT(6) NOT NULL,
+                         ReviewDescription VARCHAR(300) 
                          )";
 
-$sqlCreateUserLoginTable = "CREATE TABLE UserLogin (
-                           Username VARCHAR(30) NOT NULL UNIQUE,
-                           Password VARCHAR(50) NOT NULL, /*this isn't right need to come up with a better Type */
-                           FOREIGN KEY (UserName) REFERENCES Users(UserId),
-                           FOREIGN KEY (Password) REFERENCES Users(Password)
-                           )";
-/*Not sure about this last table */
-$sqlEmailTable = "CREATE TABLE NewsLetterEmail (
-                               EmailId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                               Email NVARCHAR (255) NOT NULL
-)";
+                            // FOREIGN KEY (UserID) REFERENCES Users(UserID),
 
-if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
+// there is no need for the commented tables
+//===================================
+// $sqlCreateUserLoginTable = "CREATE TABLE UserLogin (
+//                            Username VARCHAR(30) NOT NULL UNIQUE,
+//                            Password VARCHAR(50) NOT NULL, /*this isn't right need to come up with a better Type */
+//                            FOREIGN KEY (UserName) REFERENCES Users(UserId),
+//                            FOREIGN KEY (Password) REFERENCES Users(Password)
+//                            )";
+
+
+/*Not sure about this last table */
+// $sqlEmailTable = "CREATE TABLE NewsLetterEmail (
+//                                EmailId INT(6) NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//                                Email NVARCHAR (255) NOT NULL
+// )";
+
+
+function createTable($sql, $tablename) {
+
+  global $conn;
+  if ($conn->query($sql) === TRUE) {
+    echo "successfully created table " . $tablename;
+  } else {
+    echo "failed to create " . $tablename . "\n" . $conn->error; 
+  }
+
+  echo "<br/>";
+
 }
 
+function dropTable($tablename) {
+
+  global $conn;
+  $sql = "DROP TABLE " . $tablename;
+
+  if ($conn->query($sql) === TRUE) {
+    echo "successfully removed table " . $tablename;
+  } else {
+    echo "failed to drop " . $tablename . "<br/>". $conn->query; 
+  }
+
+  echo "<br/>";
+}
+
+// createTable($sqlCreateAttractionsTable, "Attractions");
+// createTable($sqlCreateUsersTable, "Users");
+// createTable($sqlCreateReviewTable, "Reviews");
+// dropTable("Attractions")
+// dropTable("Users")
+// dropTable("Reviews)
 
 ?>
